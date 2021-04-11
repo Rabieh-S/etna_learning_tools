94e110ebfa6c26f244e378bd6038e9710f60d1e8
sassi_r <sassi_r@etna-alternance.net>
2021-04-11 10:14:09 -0400

    SQL command

--- mysql_commands/stage13/stage13.sql
+++ mysql_commands/stage13/stage13.sql
@@ -1,8 +1,3 @@
 SELECT locations.identifier AS ville
 FROM locations
 WHERE identifier LIKE 'little%';
-
-
-SELECT ASCII('R')  AS crypt, ASCII('E') AS crypt
-FROM pokemon 
-WHERE identifier = "regice";
